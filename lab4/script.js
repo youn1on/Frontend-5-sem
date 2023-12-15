0d53abce71dc831fbdadec5de06525bf1d1e7eea
youn1on <a.shymanska.ip13@kpi.ua>
2023-12-15 02:00:22 +0200

    Subtask 1 done

---
+++ lab4/script.js
@@ -0,0 +1,19 @@
+﻿const birthInfo = document.getElementById('birth-info');
+const nextElement = document.querySelector('p.second-element');
+
+birthInfo.addEventListener('click', () => swapColors(birthInfo));
+nextElement.addEventListener('click', () => swapColors(nextElement));
+
+function swapColors(element){
+    if (element.classList.contains('class1')){
+        element.classList.remove('class1');
+        element.classList.add('class2');
+    }
+    else if (element.classList.contains('class2')){
+        element.classList.remove('class2');
+        element.classList.add('class1');
+    }
+    else{
+        element.classList.add(element === birthInfo ? 'class1' : 'class2');
+    }
+}
